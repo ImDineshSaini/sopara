a53ccd8a7b30c45c00fcffff2c0678dc7aa6000c
Dinesh Saini <dinesh.saini.x@gmail.com>
2021-06-09 15:38:24 +0530

    Filter query for search results based on actoor

--- app/controllers/tv_series_controller.rb
+++ app/controllers/tv_series_controller.rb
@@ -3,10 +3,11 @@ class TvSeriesController < ApplicationController
   def index
     @query = ''
     if params[:query].present?
-      @query = params[:query].downcase
-      @tv_series = TvSeries.joins(:actors).where(celebrities: {name: @query})
+      @query = params[:query]
+      @tv_series = TvSeries.joins(:actors).where('celebrities.name LIKE ?', "%#{@query}%")
     else
       @tv_series = TvSeries.all
     end
   end
 end
+
